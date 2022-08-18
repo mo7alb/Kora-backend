c66901dd3f4c7194546df750d634e05cb69016f9
mo7alb <mo7hammad.alb@gmail.com>
2022-08-19 03:21:07 +0400

    Added script for seeding matches to the db

--- Seed/populateMatches.js
+++ Seed/populateMatches.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const uri = process.env.MONOGO_DB_CONNECTION_URI;
-// mongoose.connect(uri, () => console.log(`Connected to db`));
+mongoose.connect(uri, () => console.log(`Connected to db`));
 
 const Match = require("../model/Match");
+const Team = require("../model/Team");
+const League = require("../model/League");
 const fs = require("fs");
 const parser = require("csv-parser");
 
@@ -14,11 +16,47 @@ const path = `${__dirname}/data/2022-23 pm season.csv`;
 function populate(fileName = path) {
    fs.createReadStream(fileName)
       .pipe(parser())
-      .on("data", function (row) {
+      .on("data", async function (row) {
          let keys = Object.keys(row);
-         console.log(keys);
 
-			
+         const league = await League.findOne({ title: "Premier League" });
+         const homeTeam = await Team.findOne({ title: row[keys[2]] });
+         const awayTeam = await Team.findOne({ title: row[keys[4]] });
+         console.log("row  ===> ", row[keys[4]]);
+         console.log("away team ===> ", awayTeam);
+         if (row[keys[3]] !== "-") {
+            // have a result
+            const match = new Match({
+               homeTeam: {
+                  team: homeTeam._id,
+                  score: parseInt(row[keys[3]]),
+               },
+               awayTeam: {
+                  team: awayTeam._id,
+                  score: parseInt(row[keys[5]]),
+               },
+               venue: row[keys[1]],
+               date: Date(row[keys[0]]),
+               league: league._id,
+            });
+
+            await match.save();
+         } else {
+            // don't have a result
+            const match = new Match({
+               homeTeam: {
+                  team: homeTeam._id,
+               },
+               awayTeam: {
+                  team: awayTeam._id,
+               },
+               venue: row[keys[1]],
+               date: Date(row[keys[0]]),
+               league: league._id,
+            });
+
+            await match.save();
+         }
       });
 }
 
